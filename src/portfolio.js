4d611ffca00dddebc3b4477a43ee6b8f9fbaa456
elvis wangari <elviswangare@gmail.com>
2024-02-03 17:07:03 +0300

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -419,15 +419,15 @@ const certifications = {
       alt_name: "Stanford University",
       color_code: "#8C151599",
     },
-    {
-      title: "Deep Learning",
-      subtitle: "- Andrew Ng",
-      logo_path: "deeplearning_ai_logo.png",
-      certificate_link:
-        "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
-      alt_name: "deeplearning.ai",
-      color_code: "#00000099",
-    },
+    // {
+    //   title: "Deep Learning",
+    //   subtitle: "- Andrew Ng",
+    //   logo_path: "deeplearning_ai_logo.png",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/specialization/H8CPSFXAJD2G",
+    //   alt_name: "deeplearning.ai",
+    //   color_code: "#00000099",
+    // },
     // {
     //   title: "ML on GCP",
     //   subtitle: "- GCP Training",
